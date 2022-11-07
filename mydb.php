48911bfe35dac9a9b8f1fa0b0ee56a8f5720e16b
ako-a <akoako.aiai@gmail.com>
2022-11-07 18:41:35 +0900

    修正内容が分かるコミットメッセージ

--- mydb.php
+++ mydb.php
@@ -1,19 +1,17 @@
 <?php
 
-$dsn = 'maaasql:dbname=mydb;host=localhost';
+$dsn = 'mysql:dbname=mydb;host=localhost';
 $user = 'root';
 $password = 'root';
-//※XAMPPを使用している場合は、passwordの中身は空にしてください.
+//※XAMPPを使用している場合は、passwordの中身は空にしてください。
 
 try {
-$dbh = new PDO($dsn, $user, $password);
-echo("接続に成功しました。<br>");
+  $dbh = new PDO($dsn, $user, $password);
+  echo("接続に成功しました。<br>");
 } catch (PDOException $e) {
-echo('Error:'.$e->getMessage());
+  echo('Error:'.$e->getMessage());
 }
 
 $dbh = null;
-
 $dbh = new PDO($dsn, $user, $password);
-
 
